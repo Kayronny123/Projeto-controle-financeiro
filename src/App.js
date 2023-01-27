954578d6384aaa362a7f3eab8ded11df884b7030
Rogerio-kayronny-sales <108687095+Kayronny123@users.noreply.github.com>
2023-01-27 15:42:58 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import GlobalStyle from "./styles/global";
+import Header from "./components/Header/index";
+import Resume from "./components/Resume";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Resume />
+      <GlobalStyle />
+    </>
+  );
+};
+export default App;
---
+++ src/components/Resume/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import * as C from "./style";
+
+const Resume = () => {
+  return <C.Container>TESTE</C.Container>;
+};
+export default Resume;
