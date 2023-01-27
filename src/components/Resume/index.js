757e2343a50ea4dce55c3d1cdd402e5638156aee
Rogerio-kayronny-sales <108687095+Kayronny123@users.noreply.github.com>
2023-01-27 19:17:05 -0300

    Criando formulario e cards de entrada saida e total

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,7 +1,19 @@
 import React from "react";
+import ResumeItem from "../ResumeItem";
 import * as C from "./style";
+import {
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaDollarSign
+} from "react-icons/fa";
 
 const Resume = () => {
-  return <C.Container>TESTE</C.Container>;
+  return (
+    <C.Container>
+      <ResumeItem title="entradas" Icon={FaRegArrowAltCircleUp} value="100" />
+      <ResumeItem title="saidas" Icon={FaRegArrowAltCircleDown} value="100" />
+      <ResumeItem title="total" Icon={FaDollarSign} value="100" />
+    </C.Container>
+  );
 };
 export default Resume;
